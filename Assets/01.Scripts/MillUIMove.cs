46046c425166b6c6a5e0322b3a6eefccfa592cfd
Anglik <seonwoo6429@gmail.com>
2021-09-29 11:59:09 +0900

    제작소 UI 만드는중

---
+++ Assets/01.Scripts/MillUIMove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MillUIMove : MonoBehaviour
+{
+    //이미지들의 크기
+    private const float bigSize = 1f;
+    private const float middleSize = 0.9f;
+    private const float smallSize = 0.75f;
+
+    //이미지들의 x위치값
+    private readonly int[] xPostions =
+    {
+        -670, -360, 0, 360, 670
+    };
+
+    //보여지는 이미지들의 위치
+    public RectTransform[] imageTrms;
+}
