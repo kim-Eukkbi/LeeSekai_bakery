27e49bf249d629afce2d2223ce5a4ff304d20d08
박선우 <seonwoo6429@gmail.com>
2021-10-20 19:50:51 +0900

    쇼케이스 선택 UI 추가

---
+++ Assets/01.Scripts/Showcase/ShowcaseUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowcaseUI : MonoBehaviour
+{
+    public List<ShowcaseItem> items = new List<ShowcaseItem>();
+
+    private void Awake()
+    {
+        
+    }
+}
