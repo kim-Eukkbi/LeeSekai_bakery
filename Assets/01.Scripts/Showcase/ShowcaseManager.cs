350b10d0060d5833f0965f1879b581856d1fe0f9
Anglik <seonwoo6429@gmail.com>
2021-11-04 11:47:22 +0900

    쇼케이스 UI 기능 연동 완료

--- Assets/01.Scripts/Showcase/ShowcaseManager.cs
+++ Assets/01.Scripts/Showcase/ShowcaseManager.cs
@@ -1,34 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum NowShowcasePanel
+{
+    Showcase,
+    Select
+}
 
 public class ShowcaseManager : MonoBehaviour
 {
     public static ShowcaseManager instance;
 
+    [HideInInspector]
     public SelectPanel selectPanel;
+
+    public Button closeButton;
 
     public CanvasGroup cvsShowcasePanel;
     public CanvasGroup csvSelectPanel;
 
+    public NowShowcasePanel nowPanel;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        selectPanel = GetComponentInChildren<SelectPanel>();
+
+        Debug.Log("SelectPanel 가져옴");
+
+        CloseSelectPanel();
+        nowPanel = NowShowcasePanel.Showcase;
+    }
+
+    private void Start()
+    {
+        closeButton.onClick.AddListener(() =>
+        {
+            switch (nowPanel)
+            {
+                case NowShowcasePanel.Showcase:
+                    Debug.Log("쇼케이스 닫힘");
+                    break;
+                case NowShowcasePanel.Select:
+                    CloseSelectPanel();
+                    break;
+            }
+        });
+    }
+
     public void OpenSelectPanel()
     {
         cvsShowcasePanel.alpha = 0;
+        cvsShowcasePanel.blocksRaycasts = false;
+
         csvSelectPanel.alpha = 1;
+        csvSelectPanel.blocksRaycasts = true;
+
+        nowPanel = NowShowcasePanel.Select;
     }
 
     public void CloseSelectPanel()
     {
         cvsShowcasePanel.alpha = 1;
-        csvSelectPanel.alpha = 0;
-    }
+        cvsShowcasePanel.blocksRaycasts = true;
 
-    private void Awake()
-    {
-        if(instance != null)
-        {
-            instance = this;
-        }
+        csvSelectPanel.alpha = 0;
+        csvSelectPanel.blocksRaycasts = false;
+
+        nowPanel = NowShowcasePanel.Showcase;
     }
 
     //쇼캐이스메니저는 selectPanel에서 선택된 빵을 관리하고 ShowcaseItem에 넣어준다
